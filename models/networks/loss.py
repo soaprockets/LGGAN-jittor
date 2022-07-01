d885437d8254f4b5a50a9037adb1440920a9c9d8
soaprockets <59349877+soaprockets@users.noreply.github.com>
2022-07-01 16:46:33 +0800

    Update loss.py

--- models/networks/loss.py
+++ models/networks/loss.py
@@ -3,10 +3,9 @@ Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 
-# import torch
 import jittor as jt
 import jittor.nn as nn
-# import jittor.functi as F
+
 from models.networks.architecture import VGG19
 
 
